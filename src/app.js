96c95e9543c4a2c5b844870f859e1c4ba6dcda99
Timo Gebauer <t-gebauer@users.noreply.github.com>
2017-08-06 17:45:30 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+const IPFS = require("ipfs")
+const Room = require("ipfs-pubsub-room")
+
+const ipfs = new IPFS({
+	repo: repo(),
+	EXPERIMENTAL: {
+		pubsub: true
+	}
+})
+
+ipfs.once("ready", () => ipfs.id((err, info) => {
+	if (err) { throw err }
+	console.log("IPFS node ready with address " + info.id)
+}))
+
+const room = Room(ipfs, "ipfs-chat-room")
+
+room.on("peer joined", (peer) => console.log("peer " + peer + " joined"))
+room.on("peer left", (peer) => console.log("peer " + peer + " left"))
+
+function repo() {
+	return "ipfs/ipfs-chat/" + Math.random()
+}
