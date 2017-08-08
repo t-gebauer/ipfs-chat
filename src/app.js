da19af98f82177026aade129e97c299c72653e93
Timo Gebauer <t-gebauer@users.noreply.github.com>
2017-08-08 19:45:42 +0200

    feat: working chat; remove react

--- src/app.js
+++ src/app.js
@@ -10,26 +10,55 @@ const ipfs = new IPFS({
 	}
 })
 
-ipfs.once("ready", () => ipfs.id((err, info) => {
-	if (err) { throw err }
-	console.log("IPFS node ready with address " + info.id)
-}))
-
-const room = Room(ipfs, "ipfs-chat-room")
-
-room.on("peer joined", (peer) => console.log("peer " + peer + " joined"))
-room.on("peer left", (peer) => console.log("peer " + peer + " left"))
-
 function repo() {
+  // random repo path -> multiple ipfs nodes in the same browser
 	return "ipfs/ipfs-chat/" + Math.random()
 }
 
-import ReactDOM from "react-dom"
-import React from "react"
+var id = undefined
 
-import App from "./components/App"
+ipfs.once("ready", () => ipfs.id((err, info) => {
+	if (err) { throw err }
+  id = info.id
+	addChatMessage({type: "system", data: "IPFS node ready with address " + info.id})
+}))
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-)
+ipfs.on("error", (err) => addChatMessage({type: "error", data: "Error: " + err}))
+
+const room = Room(ipfs, "ipfs-chat-room")
+
+room.on("peer joined", (peer) => addChatMessage({type: "system", data: "Peer " + formatName(peer) + " joined."}))
+room.on("peer left", (peer) => addChatMessage({type: "system", data: "Peer " + formatName(peer) + " left."})) 
+room.on("message", (message) => addChatMessage({type: "chat", name: message.from, data: message.data}))
+
+// Chat
+
+let chat_input = document.getElementById("chat_input")
+let chat_list = document.getElementById("chat_list")
+
+chat_input.addEventListener("keydown", (e) => {
+  if (!e) { var e = window.event }
+
+  if (e.keyCode == 13 && chat_input.value != "") {
+    room.broadcast(chat_input.value)
+    chat_input.value = ""
+  }
+})
+
+var knownIds = []
+function formatName(name) {
+  if (!name) { return "" }
+  var index = knownIds.indexOf(name)
+  if (index == -1) { index = knownIds.push(name) - 1 }
+  if (name == id) { index = "me" }
+  return index + "-[" + name.slice(-8) + "]"
+}
+
+function addChatMessage(msg) {
+  if (msg.type === undefined) { msg.type = "chat" }
+  var li = document.createElement("li")
+  li.appendChild(document.createTextNode((msg.name ? formatName(msg.name) + ": " : "") + msg.data))
+  li.className = msg.type
+  chat_list.appendChild(li)
+  chat_list.parentElement.scrollTop = chat_list.parentElement.scrollHeight
+}
