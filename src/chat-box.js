327671c9d5aef958d4d81d24c4b9b4264828f387
Timo Gebauer <t-gebauer@users.noreply.github.com>
2017-08-10 15:08:29 +0200

    feat: support chat commands

--- src/chat-box.js
+++ src/chat-box.js
@@ -5,6 +5,7 @@ class ChatBox {
     let rootNode = document.getElementById(rootNodeId)
     if (!rootNode) { throw new Error("ChatBox: can't find element by id '" + rootNodeId + "'.") }
 
+    // create elements
     let chat_list_div = document.createElement("div")
     chat_list_div.className = "chat_box_div"
     rootNode.appendChild(chat_list_div)
@@ -23,10 +24,23 @@ class ChatBox {
       if (!e) { var e = window.event }
 
       if (e.keyCode == 13 && chat_input.value != "") {
-        if (!this.messageListener) { throw new Error("ChatBox: no messageListener set!") }
-        if (this.messageListener) { this.messageListener(chat_input.value) }
+        this._onInput(chat_input.value)
         chat_input.value = ""
       }
+    })
+
+    // init commands
+    this.commands = {}
+    this.commandToken = "/"
+    this.addCommand("help", "Display this help message.", () => {
+      this.addMessage({type: "system", data: "Available commands:\n"})
+      Object.keys(this.commands).sort().forEach((k, i) => {
+        let command = this.commands[k]
+        this.addMessage({
+          type: "system",
+          data: this.commandToken + command.name + "\t" + command.description
+        })
+      })
     })
   }
 
@@ -39,8 +53,39 @@ class ChatBox {
     this.chat_list_div.scrollTop = this.chat_list_div.scrollHeight
   }
 
+  _onInput(message) {
+    if (message.startsWith(this.commandToken)) {
+      let args = message.slice(1).split(" ")
+      let name = args[0]
+      let command = this.commands[name]
+      if (command) {
+        command.fun.apply(this, args)
+      } else {
+        this.addMessage({
+          type: "system",
+          data: "Unknown command '" + name + "'. Try " + this.commandToken + "help."
+        })
+      }
+    } else {
+      if (!this.messageListener) { throw new Error("ChatBox: no messageListener set!") }
+      if (this.messageListener) { this.messageListener(message) }
+    }
+  }
+
   setMessageListener(listener) {
     this.messageListener = listener
+  }
+
+  addCommand(name, description, callback) {
+    this.commands[name] = {
+      name: name,
+      fun: callback,
+      description: description
+    }
+  }
+
+  setCommandToken(token) {
+    this.commandToken = token
   }
 }
 
